8a20bfea5674d1c626ea06fd523a412946c7cd5b
mikucool <1548701641@qq.com>
2022-07-15 13:53:11 +0800

    init

---
+++ src/main/java/com/hzz/commentbackend/CommentBackendApplication.java
@@ -0,0 +1,13 @@
+package com.hzz.commentbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommentBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommentBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hzz/commentbackend/CommentBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hzz.commentbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommentBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
