e5295226b6f01065370dc90b6b3572f8d03b5d44
mikucool <1548701641@qq.com>
2022-07-18 15:01:12 +0800

    使用 redis 缓存查询店

--- src/main/java/com/hzz/commentbackend/service/impl/ShopServiceImpl.java
+++ src/main/java/com/hzz/commentbackend/service/impl/ShopServiceImpl.java
@@ -1,15 +1,28 @@
 package com.hzz.commentbackend.service.impl;
 
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.json.JSONUtil;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.hzz.commentbackend.dto.Result;
 import com.hzz.commentbackend.entity.Shop;
 import com.hzz.commentbackend.mapper.ShopMapper;
 import com.hzz.commentbackend.service.IShopService;
+import com.hzz.commentbackend.utils.RedisConstants;
+import com.hzz.commentbackend.utils.SystemConstants;
+import org.springframework.data.geo.Distance;
+import org.springframework.data.geo.GeoResult;
+import org.springframework.data.geo.GeoResults;
+import org.springframework.data.redis.connection.RedisGeoCommands;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.domain.geo.GeoReference;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
+import java.util.*;
+
+import static com.hzz.commentbackend.utils.RedisConstants.SHOP_GEO_KEY;
 
 
 @Service
@@ -21,7 +34,25 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
 
     @Override
     public Result queryById(Long id) {
-        return Result.ok("功能未完善");
+        String key = RedisConstants.CACHE_SHOP_KEY + id;
+        // 1. 从 redis 查询商户数据
+        String shopJson = stringRedisTemplate.opsForValue().get(key);
+        // 2. 判断是否存在
+        if (StrUtil.isNotBlank(shopJson)) {
+            // 3. 存在，返回
+            Shop shop = JSONUtil.toBean(shopJson, Shop.class);
+            return Result.ok(shop);
+        }
+        // 4. redis 中不存在该商户，查询数据库
+        Shop shop = getById(id);
+        // 4.1 数据库中不存在该商户，返回错误
+        if (shop == null) {
+            return Result.fail("Not found the shop");
+        }
+        // 4.2 数据库中存在该商户，将该商户存入 redis 中
+        stringRedisTemplate.opsForValue().set(key, JSONUtil.toJsonStr(shop));
+        // 返回
+        return Result.ok(shop);
     }
 
     @Override
@@ -33,6 +64,10 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
 
     @Override
     public Result queryShopByType(Integer typeId, Integer current, Double x, Double y) {
-        return Result.ok("功能未完善");
+        Page<Shop> page = query()
+                .eq("type_id", typeId)
+                .page(new Page<>(current, SystemConstants.DEFAULT_PAGE_SIZE));
+        // 返回数据
+        return Result.ok(page.getRecords());
     }
 }
