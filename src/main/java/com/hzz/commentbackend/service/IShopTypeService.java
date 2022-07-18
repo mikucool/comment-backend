88f20bb94dc2baed175780d2b310a5f4fc23db98
mikucool <1548701641@qq.com>
2022-07-18 18:17:35 +0800

    使用 redis 给商户类型的查询做缓存

--- src/main/java/com/hzz/commentbackend/service/IShopTypeService.java
+++ src/main/java/com/hzz/commentbackend/service/IShopTypeService.java
@@ -1,8 +1,10 @@
 package com.hzz.commentbackend.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.hzz.commentbackend.dto.Result;
 import com.hzz.commentbackend.entity.ShopType;
 
 public interface IShopTypeService extends IService<ShopType> {
 
+    Result queryBySort();
 }
--- src/main/java/com/hzz/commentbackend/service/impl/ShopTypeServiceImpl.java
+++ src/main/java/com/hzz/commentbackend/service/impl/ShopTypeServiceImpl.java
@@ -1,12 +1,40 @@
 package com.hzz.commentbackend.service.impl;
 
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.json.JSONUtil;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hzz.commentbackend.dto.Result;
 import com.hzz.commentbackend.entity.ShopType;
 import com.hzz.commentbackend.mapper.ShopTypeMapper;
 import com.hzz.commentbackend.service.IShopTypeService;
+import com.hzz.commentbackend.utils.RedisConstants;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class ShopTypeServiceImpl extends ServiceImpl<ShopTypeMapper, ShopType> implements IShopTypeService {
 
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+
+    @Override
+    public Result queryBySort() {
+        String typeList = stringRedisTemplate.opsForValue().get(RedisConstants.CACHE_SHOPTYPE_KEY);
+        if (StrUtil.isNotBlank(typeList)) {
+            List<ShopType> shopTypes = JSONUtil.toList(typeList, ShopType.class);
+            return Result.ok(shopTypes);
+        }
+
+        List<ShopType> shopTypes = query().orderByAsc("sort").list();
+        if (shopTypes == null) {
+            return Result.fail("Not found the shop type");
+        }
+        stringRedisTemplate.opsForValue().set(RedisConstants.CACHE_SHOPTYPE_KEY, JSONUtil.toJsonStr(shopTypes));
+
+        return Result.ok(shopTypes);
+    }
 }
