52cc0c02993a392ae72249f3fc8a8fea3128b772
mikucool <q1548701641@gmail.com>
2022-07-21 20:15:56 +0800

    使用 lua 脚本解决释放锁时的原子性问题

--- src/main/java/com/hzz/commentbackend/utils/SimpleRedisLock.java
+++ src/main/java/com/hzz/commentbackend/utils/SimpleRedisLock.java
@@ -1,8 +1,11 @@
 package com.hzz.commentbackend.utils;
 
 import cn.hutool.core.lang.UUID;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
 
+import java.util.Collections;
 import java.util.concurrent.TimeUnit;
 
 public class SimpleRedisLock implements ILock{
@@ -11,7 +14,14 @@ public class SimpleRedisLock implements ILock{
     private StringRedisTemplate stringRedisTemplate;
     private static final String KEY_PREFIX = "lock:";
     private static final String ID_PREFIX = UUID.randomUUID().toString(true) + "-";
+    private static final DefaultRedisScript<Long> UNLOCK_SCRIPT;
+    static {
+        UNLOCK_SCRIPT = new DefaultRedisScript<>();
+        // 读取脚本文件
+        UNLOCK_SCRIPT.setLocation(new ClassPathResource("unlock.lua"));
+        UNLOCK_SCRIPT.setResultType(Long.class);
 
+    }
 
     public SimpleRedisLock(String name, StringRedisTemplate stringRedisTemplate) {
         this.name = name;
@@ -30,6 +40,15 @@ public class SimpleRedisLock implements ILock{
 
     @Override
     public void unlock() {
+        // 调用 lua 脚本
+        stringRedisTemplate.execute(UNLOCK_SCRIPT,
+                Collections.singletonList(KEY_PREFIX + name),
+                ID_PREFIX + Thread.currentThread().getId());
+
+    }
+
+    /*@Override
+    public void unlock() {
         // 获取当前的线程标识
         String threadId = ID_PREFIX + Thread.currentThread().getId();
         // 获取锁中的线程标识
@@ -40,5 +59,5 @@ public class SimpleRedisLock implements ILock{
             stringRedisTemplate.delete(KEY_PREFIX + name);
         }
         // 锁不是当前线程的，不能删除，默认不操作
-    }
+    }*/
 }
