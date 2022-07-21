dc9845375611a8824185e9383d4c3e82c916f35b
mikucool <q1548701641@gmail.com>
2022-07-21 13:43:41 +0800

    解决分布式锁的误删问题

--- src/main/java/com/hzz/commentbackend/utils/SimpleRedisLock.java
+++ src/main/java/com/hzz/commentbackend/utils/SimpleRedisLock.java
@@ -1,5 +1,6 @@
 package com.hzz.commentbackend.utils;
 
+import cn.hutool.core.lang.UUID;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
 import java.util.concurrent.TimeUnit;
@@ -8,25 +9,36 @@ public class SimpleRedisLock implements ILock{
 
     private String name;
     private StringRedisTemplate stringRedisTemplate;
+    private static final String KEY_PREFIX = "lock:";
+    private static final String ID_PREFIX = UUID.randomUUID().toString(true) + "-";
+
 
     public SimpleRedisLock(String name, StringRedisTemplate stringRedisTemplate) {
         this.name = name;
         this.stringRedisTemplate = stringRedisTemplate;
     }
 
-    private static final String KEY_PREFIX = "lock:";
 
     @Override
     public boolean tryLock(long timeoutSec) {
         // 获取线程标识，作为 value 存入 redis 作为标识
-        long threadId = Thread.currentThread().getId();
+        String threadId = ID_PREFIX + Thread.currentThread().getId();
         // 获取锁
-        Boolean success = stringRedisTemplate.opsForValue().setIfAbsent(KEY_PREFIX + name, threadId + "", timeoutSec, TimeUnit.SECONDS);
+        Boolean success = stringRedisTemplate.opsForValue().setIfAbsent(KEY_PREFIX + name, threadId, timeoutSec, TimeUnit.SECONDS);
         return Boolean.TRUE.equals(success);
     }
 
     @Override
     public void unlock() {
-        stringRedisTemplate.delete(KEY_PREFIX + name);
+        // 获取当前的线程标识
+        String threadId = ID_PREFIX + Thread.currentThread().getId();
+        // 获取锁中的线程标识
+        String lockThreadId = stringRedisTemplate.opsForValue().get(KEY_PREFIX + name);
+        // 判断标识是否一致
+        if (threadId.equals(lockThreadId)) {
+            // 锁是属于当前线程的，可以删除
+            stringRedisTemplate.delete(KEY_PREFIX + name);
+        }
+        // 锁不是当前线程的，不能删除，默认不操作
     }
 }
